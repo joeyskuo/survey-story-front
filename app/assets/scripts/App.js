80a88e8c2e3dde1757a2a55337fcf1e6b4dc8018
Unknown <joeyskuo@gmail.com>
2017-09-10 23:53:54 -0700

    finished modal behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
