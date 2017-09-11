1934bc8eb07d161f6188b85858c6b94aa541929e
Unknown <joeyskuo@gmail.com>
2017-09-10 19:45:55 -0700

    implemented behavior for header on scroll event

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+class StickyHeader {
+
+  constructor() {
+    this.siteHeader = $(".site-header");
+    this.headerTriggerElement = $(".large-hero__title");
+    this.createHeaderWaypoint();
+  }
+
+  createHeaderWaypoint() {
+    new Waypoint({
+      element: this.headerTriggerElement[0],
+      handler: (direction) => {
+        if(direction == "down") {
+          this.siteHeader.addClass("site-header--dark");
+        } else {
+          this.siteHeader.removeClass("site-header--dark");
+        }
+      }
+    });
+  }
+
+}
+
+export default StickyHeader;
